8a4db0209459592811f017156cead4ea8092d5bb
mayankboss29 <mayankkandpal18@gmail.com>
2020-06-07 23:59:07 +0530

    4th commit

--- app/src/main/java/com/example/whatsapp_clone/LoginActivity.kt
+++ app/src/main/java/com/example/whatsapp_clone/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.example.whatsapp_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        editTextPhone.addTextChangedListener {
+            nextBtn.isEnabled=!(it.isNullOrEmpty() || it.length<10)
+        }
     }
 }
