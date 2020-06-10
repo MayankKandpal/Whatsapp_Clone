4f69f79d4665de0d0aa648672050dae89f509f59
mayankboss29 <mayankkandpal18@gmail.com>
2020-06-10 23:58:02 +0530

    Login Activity added

--- app/src/main/java/com/example/whatsapp_clone/LoginActivity.kt
+++ app/src/main/java/com/example/whatsapp_clone/LoginActivity.kt
@@ -1,16 +1,50 @@
 package com.example.whatsapp_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var countrycode :String
+    private lateinit var phoneno : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        editTextPhone.addTextChangedListener {
+        editTextPhone.addTextChangedListener{
             nextBtn.isEnabled=!(it.isNullOrEmpty() || it.length<10)
         }
+        nextBtn.setOnClickListener{
+            checknumber()
+        }
+    }
+
+    private fun checknumber() {
+        countrycode = ccp.selectedCountryCodeWithPlus
+        phoneno = countrycode+editTextPhone.text.toString()
+        notifyuser()
+    }
+
+    private fun notifyuser() {
+        MaterialAlertDialogBuilder(this).apply {
+            setMessage("We are going to verify the phone no:$phoneno\n"+"Is this ok or would you like to change the number")
+           setPositiveButton("ok") { _, _ ->
+           showOtpActivity()
+           }
+           setNegativeButton("Edit"){dialog, which ->
+               dialog.dismiss()
+           }
+            setCancelable(false)
+            create()
+            show()
+
+        }
+ }
+
+    private fun showOtpActivity() {
+        startActivity(Intent(this,OtpActivity::class.java).putExtra(PHONE_NUMBER,phoneno))
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/whatsapp_clone/OtpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.whatsapp_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+const val PHONE_NUMBER = "phoneNumber"
+class OtpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
