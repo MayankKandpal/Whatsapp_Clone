7fa7e602cfe16e5a7a1b8f9ef27ee8663e685d84
mayankboss29 <mayankkandpal18@gmail.com>
2020-06-11 16:01:02 +0530

    Countdown timer added

--- app/src/main/java/com/example/whatsapp_clone/OtpActivity.kt
+++ app/src/main/java/com/example/whatsapp_clone/OtpActivity.kt
@@ -1,13 +1,16 @@
 package com.example.whatsapp_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.View
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_otp.*
 
 const val PHONE_NUMBER = "phoneNumber"
@@ -17,6 +20,22 @@ class OtpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
         initviews()
+        showTimer(60000)
+    }
+
+    private fun showTimer(milliSecInFuture: Long) {
+        resendBtn.isEnabled=false
+        object :CountDownTimer(milliSecInFuture,1000){
+            override fun onFinish() {
+                resendBtn.isEnabled=true
+                counterTv.isVisible=false
+            }
+            override fun onTick(millisUntilFinished: Long) {
+                counterTv.isVisible=true
+                counterTv.text=getString(R.string.second_remaining,millisUntilFinished/1000)
+            }
+        }.start()
+ggit
     }
 
     private fun initviews() {
@@ -30,6 +49,7 @@ class OtpActivity : AppCompatActivity() {
         val clickableSpan = object : ClickableSpan(){
             override fun onClick(widget: View) {
                 //send to previous activity
+                showLoginActivity()
             }
             override fun updateDrawState(ds: TextPaint) {
                 ds.color = ds.linkColor // you can use custom color
@@ -39,5 +59,14 @@ class OtpActivity : AppCompatActivity() {
         span.setSpan(clickableSpan,span.length-13,span.length,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         waitingTv.movementMethod = LinkMovementMethod.getInstance()
         waitingTv.text=span
+    }
+
+    private fun showLoginActivity() {
+       startActivity(Intent(this,LoginActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+       //flag helps us in removing/clearing the back stack
+    }
+
+    override fun onBackPressed() {
+        //removing the super function helps us in disabling the action of pressing of back button
     }
 }
